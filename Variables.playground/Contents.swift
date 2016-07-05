8afe6b6d30a0a62f627599b2807ac189f3bfa02c
ellscha <escharlin@wesleyan.edu>
2016-07-05 13:51:27 -0400

    changed the file.  no more errrors

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -4,9 +4,9 @@ print(favoriteCharacter)
 favoriteCharacter = "Tyrion Lannister"
 print(favoriteCharacter)
 
-favoriteCharacter = 76
+//favoriteCharacter = "76" would solve the error.
 
 let ultimateFavoriteCharacter = "Arya Stark"
 print(ultimateFavoriteCharacter)
 
-ultimateFavoriteCharacter = "Daenerys Targaryen"
+//ultimateFavoriteCharacter = "Daenerys Targaryen" since it was declared as a constant, we cannot change the value, also Arya is my favorite character so I don't want to make it Daenerys even though she's really cool. I don't want to make ultimateFavoriteCharacter a 'var'.
